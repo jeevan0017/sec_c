5496a5e55e2c78ddbcad9989ccfb050f5b01dd64
jeevan0017 <jeevan.p@s.amity.edu>
2025-07-14 12:15:09 +0530

    written checking of even or odd code

--- jsp.c
+++ jsp.c
@@ -2,5 +2,16 @@
 int main()
 {
 	printf("Hello,I'm Jeevan");
+	int num;
+	printf("Enter the first num:");
+	scan("%d",&sum);
+	if(num % 2==0)
+	{
+		printf("it's even num");
+	}
+	else
+	{
+		print("it's odd")
+	}	
 	return 0;
 }	

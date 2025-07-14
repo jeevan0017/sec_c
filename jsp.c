a32791680a8b7d5652f1d4896181592563f90302
jeevan0017 <jeevan.p@s.amity.edu>
2025-07-14 12:27:47 +0530

    Copyed Swaping of two num code from br3

--- jsp.c
+++ jsp.c
@@ -2,5 +2,15 @@
 int main()
 {
 	printf("Hello,I'm Jeevan");
+
+	 int a,int b,temp;
+        printf("Enter two num:");
+        scan("%d %d",&a,&b);
+        temp=a;
+        a=b;
+        b=temp;
+        printf("After swap: a=%d,b=%d\n",a,b);
+
+
 	return 0;
 }	
